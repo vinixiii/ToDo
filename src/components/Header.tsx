3a40770038ad601cdf5d0797436b6ebf28927429
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2021-12-22 13:53:42 -0300

    feat: challenge completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
@@ -10,6 +10,11 @@ interface HeaderProps {
 
 export function Header({ tasksCounter }: HeaderProps) {
   // const tasksCounterText = TODO render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
+  const [tasksCounterText, setTasksCounterText] = useState('');
+
+  useEffect(() => {
+    setTasksCounterText(tasksCounter === 1 ? 'tarefa' : 'tarefas');
+  },[tasksCounter])
   
   return (
     <View style={styles.container}>
@@ -17,7 +22,7 @@ export function Header({ tasksCounter }: HeaderProps) {
       
       <View style={styles.tasks}>
         <Text style={styles.tasksCounter}>Você tem </Text>
-        {/* <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text> */}
+        <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text>
       </View>
     </View>
   )
